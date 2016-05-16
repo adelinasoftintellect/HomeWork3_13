18faba851bec5d5985ae7a741b5b5322479d66fa
Adi <adelina_yordanova11@abv.bg>
2016-05-16 07:14:00 +0300

    Homework3_13.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int number;
+		int max = 0;
+		int min = 0;
+
+		for (int x = 0; x < 5; x++) {
+			System.out.print("Please, enter an integer: ");
+			number = input.nextInt();
+
+			if (number > max) {
+				max = number;
+			}
+			if (number < min) {
+				min = number;
+			}
+		}
+		System.out.println("Highest value: " + max);
+		System.out.println("Smallest value: " + min);
+
+		input.close();
+	}
+}
